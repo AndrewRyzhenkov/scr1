c680c8f11ddaf96d01c636ca01e7ed1534f7a7ad
Andrew Ryzhenkov <ryzhenkov.an@mail.ru>
2025-04-30 21:24:20 +0300

    add lab3

---
+++ src/tb/scr1_tb_log_cmd.sv
@@ -0,0 +1,17 @@
+module scr1_tb_log_cmd();
+ 
+
+always_ff @(posedge scr1_top_tb_ahb.i_top.i_imem_ahb.clk) begin
+
+    if (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_resp == 2'b01) begin
+        // valid data from ahb router
+        if (
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[6 : 0] == 7'b1100011) &
+            (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[14 : 12] == 3'b001)
+        ) begin
+            // detect and command
+            $display("curr_pc = %h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.curr_pc);
+        end
+    end
+end
+endmodule
